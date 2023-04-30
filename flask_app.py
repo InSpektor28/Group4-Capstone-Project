b9af76db92921c0244556985f3de214c1e5802c1
Spektor <blazedup20@gmail.com>
2023-04-30 16:18:00 -0400

    Initial commit

---
+++ flask_app.py
@@ -0,0 +1,108 @@
+from flask import Flask, Response, render_template, request, redirect, url_for, send_from_directory
+from flask_socketio import SocketIO, emit, join_room
+import random
+import secrets
+from werkzeug.utils import secure_filename
+import os
+
+app = Flask(__name__, static_url_path='/static')
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+rooms = {}
+
+
+def get_rooms():
+    while True:
+        room_id = secrets.token_hex(16)
+        if room_id not in rooms:
+            return room_id
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/nav-link')
+def nav_link():
+    return render_template('code_link.html')
+
+
+@app.route('/play_video', methods=['POST'])
+def play_video():
+    video = request.files['video']
+    if video and allowed_file(video.filename):
+        filename = secure_filename(video.filename)
+        video_path = os.path.join('uploads', filename)
+        video.save(video_path)
+        video_url = url_for('uploaded_file', filename=filename)
+        room_id = request.form.get('room_id')
+        video_mime = 'video/mp4'
+        return render_template('index.html', room_id=room_id, video_url=video_url, video_mime=video_mime, filename=filename)
+    else:
+        return "File not supported. Please upload a video file."
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory('uploads', filename)
+
+@socketio.on('play_video')
+def handle_play_video():
+    print("Received play_video event")
+    emit('play_video', broadcast=True)
+
+
+@socketio.on('pause_video')
+def handle_pause_video():
+    emit('pause_video', broadcast=True)
+
+
+@socketio.on('seek_video')
+def handle_seek_video(time):
+    emit('seek_video', time, broadcast=True)
+
+allowed_extensions = ['mp4']
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in allowed_extensions
+
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    if 'video' not in request.files:
+        return 'No video file found'
+    video = request.files['video']
+    if video.filename == '':
+        return 'No video selected'
+    if video and allowed_file(video.filename):
+        video.save('static/videos/' + video.filename)
+        return render_template('index.html', video_name=video.filename)
+    return 'Invalid video file'
+
+
+@app.route('/generate_url')
+def generate_url():
+    unique_id = secrets.token_hex(16)
+    return render_template('code_link.html', unique_id=unique_id)
+
+
+@app.route('/watch_with_others/<room_id>')
+def watch_with_others(room_id):
+    video_url = rooms[room_id]
+    video_mime = 'video/mp4'
+    return render_template('index.html', room_id=room_id, video_url=video_url, video_mime=video_mime)
+
+
+@socketio.on('send_message')
+def handle_send_message(data):
+    room_id = data['room_id']
+    message = data['message']
+    username = data['username']
+    join_room(room_id)
+    print(f"Received message from user {username} in room {room_id}: {message}")
+    emit('receive_message', {'username': username, 'message': message}, room=room_id)
+
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
---
+++ static/js/chat.js
@@ -0,0 +1,82 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var socket = io();
+    var chatForm = document.getElementById('chat-form');
+    var chatInput = document.getElementById('chat-input');
+    var chatContainer = document.getElementById('chat-container');
+    var chatMessages = document.getElementById('chat-messages');
+    var hiddenRoomId = document.getElementById('room_id');
+
+    chatForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+        sendMessage();
+    });
+
+    function sendMessage() {
+        var message = chatInput.value;
+        var username = document.getElementById('username-input').value || 'Anonymous';
+        var room_id = document.getElementById('room_id').value;
+        if (message) {
+            console.log('Sending message:', message, 'from user:', username, 'to room:', room_id);
+            socket.emit('send_message', {username: username, message: message, room_id: room_id});
+            chatInput.value = '';
+        }
+    }
+
+    function addMessageToChat(username, message) {
+        const chatMessages = document.getElementById('chat-messages');
+        const chatMessage = document.createElement('p');
+        chatMessage.classList.add('chat-message');
+
+        const usernameSpan = document.createElement('span');
+        usernameSpan.classList.add('username');
+        usernameSpan.textContent = username + ': ';
+
+        const messageSpan = document.createElement('span');
+        messageSpan.textContent = message;
+
+        chatMessage.appendChild(usernameSpan);
+        chatMessage.appendChild(messageSpan);
+        chatMessages.appendChild(chatMessage);
+
+        chatMessages.scrollTop = chatMessages.scrollHeight;
+    }
+
+    socket.on('receive_message', function(data) {
+        console.log("Message event received on client side");
+        console.log("Received message:", data.message, "from user:", data.username, "in room:", data.room_id);
+        
+        // Call the addMessageToChat function here
+        addMessageToChat(data.username, data.message);
+    });
+
+    // Video synchronization
+    socket.on('play_video', function() {
+        console.log("Play video event received on client side");
+
+        const videoPlayer = document.getElementById('video_url');
+        videoPlayer.play();
+    });
+
+    socket.on('pause_video', function() {
+        const videoPlayer = document.getElementById('video_url');
+        videoPlayer.pause();
+    });
+
+    socket.on('seek_video', function(time) {
+        const videoPlayer = document.getElementById('video_url');
+        videoPlayer.currentTime = time;
+    });
+
+    const videoPlayer = document.getElementById('video_url');
+    videoPlayer.addEventListener('play', function() {
+        socket.emit('play_video');
+    });
+
+    videoPlayer.addEventListener('pause', function() {
+        socket.emit('pause_video');
+    });
+
+    videoPlayer.addEventListener('seeked', function() {
+        socket.emit('seek_video', videoPlayer.currentTime);
+    });
+});
